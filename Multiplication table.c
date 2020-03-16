ea4f79f99763f8efef98fc2e87aa8a8397ef2efc
vishnuvs0987654321 <62145261+vishnuvs0987654321@users.noreply.github.com>
2020-03-16 14:20:47 +0530

    Create Multiplication table.c

---
+++ Multiplication table.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+int main()
+  {
+     int num, i = 1;
+     printf("Enter any Number:");
+     scanf("%d", &num);
+     printf("Multiplication table of %d: ", num);
+    
+     while (i <= 10) 
+       {
+        printf("
+        %d x %d = %d", num, i, num * i);
+        i++;
+       }
+    return 0;
+   }
